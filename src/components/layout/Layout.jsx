596c6fab4f166fce57b775344f023104f84009a6
Eunhye Jeong <gracejelly125@gmail.com>
2024-11-17 18:30:51 +0900

    layout 시맨틱 태그 및 styled 적용

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,66 +1,112 @@
-const HeaderStyles = {
-  width: '100%',
-  background: 'gray',
-  height: '50px',
-  display: 'flex',
-  alignItems: 'center',
-  paddingLeft: '20px',
-  color: 'white',
-  fontWeight: '600',
-  gap: '20px'
-}
-const FooterStyles = {
-  width: '100%',
-  height: '50px',
-  display: 'flex',
-  background: 'gray',
-  paddingLeft: '20px',
-  color: 'white',
-  alignItems: 'center',
-  justifyContent: 'start',
-  fontSize: '12px'
-}
-const layoutStyles = {
-  display: 'flex',
-  flexDirection: 'column',
-  justifyContent: 'center',
-  alignItems: 'center',
-  minHeight: '90vh'
-}
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+
 function Header() {
   return (
-    <div style={{ ...HeaderStyles }}>
-      <img style={{ width: '50px' }} src="/images/logo.png" />
-      <span>목록</span>
-      <span>소개</span>
-      <div
-        style={{
-          marginLeft: 'auto',
-          paddingRight: '20px',
-          display: 'flex',
-          gap: '10px'
-        }}
-      >
-        <button>로그인</button>
-        <button>회원가입</button>
-      </div>
-    </div>
-  )
+    <HeaderContainer>
+      <nav>
+        <img src="/images/logo.png" alt="Logo" />
+        <ul>
+          <li>
+            <StyledLink to="/about">소개</StyledLink>
+          </li>
+          <li>
+            <StyledLink to="/list">목록</StyledLink>
+          </li>
+        </ul>
+        <div className="nav-links">
+          <StyledLink to="/login">로그인</StyledLink>
+          <StyledLink to="/signup">회원가입</StyledLink>
+        </div>
+      </nav>
+    </HeaderContainer>
+  );
 }
+
 function Footer() {
   return (
-    <div style={{ ...FooterStyles }}>
+    <FooterContainer>
       <span>© 2024 아재슐랭. All rights reserved.</span>
-    </div>
-  )
+    </FooterContainer>
+  );
 }
+
 function Layout({ children }) {
   return (
     <div>
       <Header />
-      <div style={{ ...layoutStyles }}>{children}</div>
+      <LayoutContainer>{children}</LayoutContainer>
       <Footer />
     </div>
-  )
+  );
 }
+
 export default Layout;
+
+const HeaderContainer = styled.header`
+  width: 100%;
+  background: gray;
+  height: 50px;
+  display: flex;
+  align-items: center;
+  padding-left: 20px;
+  color: white;
+  font-weight: 600;
+  gap: 20px;
+
+  nav {
+    display: flex;
+    align-items: center;
+    width: 100%;
+  }
+
+  img {
+    width: 50px;
+    margin-right: 10px;
+  }
+
+  ul {
+    display: flex;
+    list-style: none;
+    margin: 0;
+    padding: 0;
+    gap: 10px;
+  }
+
+  li {
+    display: flex;
+    align-items: center;
+  }
+
+  .nav-links {
+    margin-left: auto;
+    padding-right: 20px;
+    display: flex;
+    gap: 10px;
+  }
+`;
+
+const StyledLink = styled(Link)`
+  text-decoration: none;
+  color: white;
+`;
+
+const FooterContainer = styled.footer`
+  width: 100%;
+  height: 50px;
+  display: flex;
+  background: gray;
+  padding-left: 20px;
+  color: white;
+  align-items: center;
+  justify-content: start;
+  font-size: 12px;
+`;
+
+const LayoutContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  min-height: 90vh;
+`;
