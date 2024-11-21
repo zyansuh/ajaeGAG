54c3a05c56622f1ca6262fa20cf0892ebdef8345
kminhoo <whdgus4158@gmail.com>
2024-11-21 09:28:26 +0900

    일부 수정

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client' // 수정된 import 경로
 import App from './App'
-// import { UserContextProvider } from './context/userContext'
+import { UserContextProvider } from './context/userContext'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
